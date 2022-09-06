a8cb2e441f1557746c6f1a2da5203e9f57046b35
minarul hoque <minarulak88@gmail.com>
2022-09-06 16:22:27 +0530

    location get from ip

--- src/app.js
+++ src/app.js
@@ -5,28 +5,47 @@ class App {
     this._getPosition();
   }
   _getPosition() {
-    navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), () => {
-      alert("please give permision");
-    });
+    navigator.geolocation.getCurrentPosition(this._loadMap.bind(this),this._locateUsingIp.bind(this));
   }
   _loadMap(position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
     this.#map = L.map("map").setView([latitude, longitude], 12);
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "© OpenStreetMap",
     }).addTo(this.#map);
     L.marker([latitude, longitude])
-      .on('click',this._showDetails)
+      .on("click", this._showDetails)
       .addTo(this.#map)
       .bindPopup(
         L.popup({
           className: "location",
           maxWidth: 250,
           minWidth: 200,
-        }).setContent("you are here"),
-      )
+        }).setContent("you are here")
+      );
+    this.#map.on("click", this._showDetails.bind(this));
+  }
+  async _locateUsingIp(){
+    let res = await fetch("https://ipapi.co/json/");
+    let location = await res.json();
+    console.log(location);
+    const  latitude  = location.latitude
+    const  longitude  = location.longitude;    
+    this.#map = L.map("map").setView([latitude, longitude], 12);
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "© OpenStreetMap",
+    }).addTo(this.#map);
+    L.marker([latitude, longitude])
+      .on("click", this._showDetails)
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          className: "location",
+          maxWidth: 250,
+          minWidth: 200,
+        }).setContent("you are here")
+      );
     this.#map.on("click", this._showDetails.bind(this));
   }
   _showDetails(e) {
