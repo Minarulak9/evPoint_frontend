40b0e63a9c63303e47fd228fcafef8f157dd3e41
minarul hoque <minarulak88@gmail.com>
2022-09-07 08:22:42 +0530

    added search bar

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 class App {
   #map;
-  #mavEvent;
+  #search;
+  #evs;
   constructor() {
     this._getPosition();
   }
   _getPosition() {
-    navigator.geolocation.getCurrentPosition(this._loadMap.bind(this),this._locateUsingIp.bind(this));
+    navigator.geolocation.getCurrentPosition(
+      this._loadMap.bind(this),
+      this._locateUsingIp.bind(this)
+    );
   }
   _loadMap(position) {
     const { latitude } = position.coords;
@@ -25,36 +29,53 @@ class App {
         }).setContent("You are within 6.4705 meters from this point")
       );
     this.#map.on("click", this._showDetails.bind(this));
-    L.Control.geocoder().addTo(this.#map);
+    var geocoder = L.Control.geocoder({
+      defaultMarkGeocode: false,
+    })
+      .on("markgeocode", this._markSearch.bind(this))
+      .addTo(this.#map);
   }
-  async _locateUsingIp(){
+  async _locateUsingIp() {
     let res = await fetch("https://ipapi.co/json/");
-    // let res = await fetch("https://api.bigdatacloud.net/data/ip-geolocation?key=bdc_aee86d43bdd846afa5d0b23096b40b58");
     let location = await res.json();
-    console.log(location);
-    const  latitude  = location.latitude
-    const  longitude  = location.longitude;    
-    this.#map = L.map("map").setView([latitude, longitude], 12);
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution: "© OpenStreetMap",
-    }).addTo(this.#map);
-    L.marker([latitude, longitude])
-      .on("click", this._showDetails)
-      .addTo(this.#map)
-      .bindPopup(
-        L.popup({
-          className: "location",
-          maxWidth: 250,
-          minWidth: 200,
-        }).setContent("You are within 100km from this point. give location permission to get accurate location?")
-      );
-    this.#map.on("click", this._showDetails.bind(this));
-    L.Control.geocoder().addTo(this.#map);
+    const pos = {
+      coords: {
+        latitude: location.latitude,
+        longitude: location.longitude,
+      },
+    };
+    this._loadMap.bind(this)(pos);
   }
   _showDetails(e) {
     const detailsElm = document.querySelector("#side_bar");
     detailsElm.classList.toggle("active");
   }
+  _markSearch(e) {
+    var bbox = e.geocode.bbox;
+    this.#search?.remove();
+    this.#search = L.polygon([
+      bbox.getSouthEast(),
+      bbox.getNorthEast(),
+      bbox.getNorthWest(),
+      bbox.getSouthWest(),
+    ]).addTo(this.#map);
+    this.#map.fitBounds(this.#search.getBounds());
+  }
+  markEvs(coords) {
+    console.log(this);
+    coords.forEach((c) => {
+      console.log(c);
+      console.log(this.#map);
+      this.#evs = L.marker([c.latitude, c.longitude])
+        .addTo(this.#map)
+        .bindPopup(
+          L.popup({
+            className: "location",
+            maxWidth: 250,
+            minWidth: 200,
+          }).setContent("ev station")
+        );
+    });
+  }
 }
 const app = new App();
-const mapElm = document.querySelector("#map");
